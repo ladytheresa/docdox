2229bc9d2b5aa67afd1140bb699081ef6efe332c
joyamadea <amadeajoy@gmail.com>
2020-11-18 10:44:03 +0700

    Add logo + text; Change navbar color; UI Fixes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"],
 })
 export class HomeComponent implements OnInit {
+  fakeGroup = Array(3);
+  clicked: boolean = false;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  oneClick(){
+    console.log('one click');
   }
 
+  doubleClick(){
+    console.log('double click');
+  }
 }
