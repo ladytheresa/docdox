d528d6a1e62817e5d996a4c088c33c35370c2732
ladytheresa <ladytheresa65@gmail.com>
2020-11-18 17:42:51 +0700

    changed button and added login

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,12 +8,27 @@ import { Component, OnInit } from "@angular/core";
 export class HomeComponent implements OnInit {
   fakeGroup = Array(3);
   clicked: boolean = false;
+  selected: string;
   constructor() {}
 
   ngOnInit(): void {}
 
   oneClick(){
     console.log('one click');
+  }
+
+  selectGroup(event){
+    let target = event.target;
+    if (target.nodeName === "BUTTON"){
+      let isCertainButtonAlreadyActive = target.parentElement.querySelector(".active");
+      if( isCertainButtonAlreadyActive ) {
+        isCertainButtonAlreadyActive.classList.remove("active");
+      }
+      else{  
+        target.className += " active";
+      }
+    }
+    console.log(target);
   }
 
   doubleClick(){
