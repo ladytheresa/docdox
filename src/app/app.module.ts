1ecac611fb6a91db0d849676fc8ccda791aee79c
ladytheresa <ladytheresa65@gmail.com>
2020-12-01 18:53:49 +0700

    pdf viewer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
 
 @NgModule({
   declarations: [
@@ -14,7 +14,8 @@ import { LoginComponent } from './login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxExtendedPdfViewerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { PdfViewerModule } from 'ng2-pdf-viewer';
 
 @Component({
   selector: "app-home",
@@ -8,15 +9,39 @@ import { Component, OnInit } from "@angular/core";
 export class HomeComponent implements OnInit {
   fakeGroup = Array(3);
   clicked: boolean = false;
+  docdetails:boolean=false;
+  groupdetails:boolean=false;
   selected: string;
   private selectedFile: File;
   filename: string = "";
+  pdfSrc = "";
+  isVisible: boolean = false;  
+
   constructor() {}
 
   ngOnInit(): void {}
 
+  moveUp(index: number) {
+    console.log("up", this.fakeGroup[index]);
+    if (index >= 1)
+      this.swap(index, index - 1)
+  }
+
+  moveDown(index: number) {
+    console.log("down", this.fakeGroup[index])
+    if(index < this.fakeGroup.length-1)
+    this.swap(index, index + 1)
+  }
+  private swap(x: any, y: any) {
+    var b = this.fakeGroup[x];
+    this.fakeGroup[x] = this.fakeGroup[y];
+    this.fakeGroup[y] = b;
+  }
+
   oneClick(){
     console.log('one click');
+    this.docdetails = true;
+    this.groupdetails=false;
   }
 
   onFileSelect(event) {
@@ -26,6 +51,8 @@ export class HomeComponent implements OnInit {
   }
 
   selectGroup(event){
+    this.docdetails = false;
+    this.groupdetails=true;
     let target = event.target;
     if (target.nodeName === "BUTTON"){
       let isCertainButtonAlreadyActive = target.parentElement.querySelector(".active");
@@ -41,5 +68,18 @@ export class HomeComponent implements OnInit {
 
   doubleClick(){
     console.log('double click');
+    //this.openDoc('assets/dummy/00000020013_JoyAmadea_ProposalSkripsi.pdf',1); this is for  new tab
+    this.pdfSrc="temp/lorem-ipsum.pdf";
+    document.getElementById("openPreviewButton").click();
+    //this.isVisible = true;
   }
+
+  /*containerClicked(){
+    if(this.isVisible==true){
+      this.isVisible=false;
+    }
+  }*/
+  /*openDoc(pdfUrl: string, startPage: number ) {
+    window.open(pdfUrl + '#page=' + startPage, '_blank', '', true);
+  }*/
 }
